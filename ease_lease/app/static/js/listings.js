fb4a3bae10a4b5de632b1d9faf7eda8fb01aeaed
owen zhang <zhangsz.owen@gmail.com>
2024-04-15 22:56:19 +0000

    finished filter

---
+++ ease_lease/app/static/js/listings.js
@@ -0,0 +1,29 @@
+document.getElementById('filter-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const searchParams = new URLSearchParams(formData);
+
+    fetch(`/listings?${searchParams.toString()}`) 
+      .then(response => response.json())
+      .then(data => {
+        updateListings(data);
+      });
+  });
+
+  function updateListings(listings) {
+    const listingsContainer = document.getElementById('listings-container');
+    listingsContainer.innerHTML = '';
+
+    listings.forEach(listing => {
+      const listingElement = document.createElement('li');
+      listingElement.innerHTML = `
+        <h2>${listing.room_type}</h2>
+        <p>Description: ${listing.description}</p>
+        <p>Rating: ${listing.scores_rating}</p>
+        <p>Price $: ${listing.price}</p>
+        <p>Available from: ${listing.from_date} to ${listing.to_date}</p>
+    `;
+      listingsContainer.appendChild(listingElement);
+    });
+  }
--- ease_lease/routes/listings_routes.py
+++ ease_lease/routes/listings_routes.py
@@ -1,7 +1,20 @@
-from flask import render_template
-from utils.listings_utils import get_top_listings
+from flask import render_template, request, jsonify
+from utils.listings_utils import get_top_listings, get_filtered_listings
 from app import app 
 @app.route('/listings', methods=['GET'])
 def listings():
-    top_listings = get_top_listings()
-    return render_template('listings.html', listings=top_listings)
+    if request.args:
+        max_price = request.args.get('max_price')
+        room_type = request.args.get('room_type')
+        start_date = request.args.get('start_date')
+        end_date = request.args.get('end_date')
+        min_rating = request.args.get('min_rating')
+        filtered_listings = get_filtered_listings(max_price, room_type, start_date, end_date, min_rating)
+        for listing in filtered_listings:
+            listing['from_date'] = listing['from_date'].strftime('%Y-%m-%d')
+            listing['to_date'] = listing['to_date'].strftime('%Y-%m-%d')
+        return jsonify(filtered_listings)
+    else:
+        top_listings = get_top_listings()
+        print(top_listings)
+        return render_template('listings.html', listings=top_listings)
